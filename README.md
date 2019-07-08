b8e51bc41447b4e741bf558fa3d067fb781e6c7f
David Gomez <hi@davidgomezgam.com>
2019-07-08 10:14:04 +0000

    Change name player

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# rock-paper-scissors
-Simple Game - Rock Paper Scissors
+# Rock Paper Scissors(rock-paper-scissors)
+Rock Paper Scissors is a simple game in PHP console. 
+
+## Installation
+
+```bash
+composer install
+```
+
+## Usage
+```bash
+php game.php
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- game.php
+++ game.php
@@ -6,7 +6,7 @@ $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
 $player1 = new App\Players\Player('David Gomez');
-$player2 = new App\Players\Player('HelpMyCash', false);
+$player2 = new App\Players\Player('Player 2', false);
 $player2->setWeapon(new App\Weapons\Rock);
 
 $battle = new App\Game\Game(getenv('ROUNDS'), $player1, $player2);
